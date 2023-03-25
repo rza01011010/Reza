0aa5ecb4304bf3ebf4cf95a833d1f3e29718d11b
rza01011010 <128021914+rza01011010@users.noreply.github.com>
2023-03-25 14:44:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Nama : Reza Ata Fadhillah</p><p>Tempat, Tanggal Lahir : Jakarta, 23 Mei 2004</p><p>Agama : Islam</p><p>Alamat : Jl. Bukit Tanggul</p><p>Jenis Kelamin : Laki-laki
-</p><p>Umur : 19 tahun</p>"
+  return "<p>Nama : Reza Ata Fadhillah</p><p>Tempat, Tanggal Lahir : Jakarta, 23 Mei 2004</p><p>Agama : Islam</p><p>Alamat : Jl. Bukit Tanggul</p><p>Jenis Kelamin : Laki-laki</p><p>Umur : 19 tahun</p>"
 
         
 if __name__ == "__main__":
