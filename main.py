c454f0cb24dd9a6f97e77d69129e90c12b7023f6
rza01011010 <128021914+rza01011010@users.noreply.github.com>
2023-03-25 14:40:37 +0700

    Biodata

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Reza Ata Fadhillah</p>"
+  return "<p>Nama : Reza Ata Fadhillah</p>"<p>Tempat, Tanggal Lahir : Jakarta, 23 Mei 2004</p><p>Agama : Islam</p><p>Alamat : Jl. Bukit Tanggul</p><p>Jenis Kelamin : </p><p>Umur : 19 tahun</p>"
 
-
+        
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
